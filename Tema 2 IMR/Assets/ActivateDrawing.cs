489666518101dee5fdcada2eb8bafe637a917707
chaivas <72473472+chaivas@users.noreply.github.com>
2020-10-14 18:52:55 +0300

    Chalk drawing

---
+++ Tema 2 IMR/Assets/ActivateDrawing.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateDrawing : MonoBehaviour
+{
+
+    private TrailRenderer trail;
+    // Start is called before the first frame update
+    void Start()
+    {
+        trail = GetComponent<TrailRenderer>();
+        trail.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            trail.enabled = !trail.enabled;
+        }
+        
+    }
+}
